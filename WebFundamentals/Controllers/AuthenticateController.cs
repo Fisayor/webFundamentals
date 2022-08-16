0886152919bad1905fd743eb5d2ac7735bb8cdbc
Fisayor <oluwafisayoowoyele@gmail.com>
2022-08-16 02:03:39 -0700

    first commit

---
+++ WebFundamentals/Controllers/AuthenticateController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace WebFundamentals.Controllers
+{
+    public class AuthenticateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login( string username)
+        {
+            username = "uche";
+
+            var appClaims = new List<Claim>()
+
+                    {
+                          new Claim(ClaimTypes.Name,username),
+                          
+                          //new Claim(ClaimTypes.Email,user.Email),
+                          //new Claim("app.Says","A cool app"),
+                    };
+
+            var appIdentity = new ClaimsIdentity(appClaims, "App Identity");
+
+            var userPrincipal = new ClaimsPrincipal(new[] { appIdentity });
+
+            HttpContext.SignInAsync(userPrincipal);
+
+
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync();
+
+
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
