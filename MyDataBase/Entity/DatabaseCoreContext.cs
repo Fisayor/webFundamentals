fb3195d7fc9b21dcebba553a1dc65fad8e37ee98
Fisayor <oluwafisayoowoyele@gmail.com>
2022-08-16 03:31:21 -0700

    second commit

--- MyDataBase/Entity/DatabaseCoreContext.cs
+++ MyDataBase/Entity/DatabaseCoreContext.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +15,31 @@ namespace MyDataBase.Entity
         public DatabaseCoreContext(){
 
 
+        }
+
+        public DatabaseCoreContext(DbContextOptions<DatabaseCoreContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location))
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false);
+
+            IConfiguration config = builder.Build();
+            var connection = config.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connection);
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        //mapping it
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { 
+            //whatever thing that exist in the Users table, use it in the User table
+        modelBuilder.Entity<User>().ToTable("Users");
         }
     }
 }
---
+++ MyDataBase/Entity/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDataBase.Entity
+{
+    public class User
+    {
+        public string UserId { get; set; }
+
+        //question mark means it can accept null
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Email { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime LastModified { get; set; }  
+
+       
+    }
+}
